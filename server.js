e346f2cfe1506d72e6bb8c6f4ffed4801f72b5b7
Fourbits <fourbits@gmail.com>
2016-06-16 02:47:41 +0000

    check for missing info

--- server.js
+++ server.js
@@ -3,26 +3,37 @@ var app = express()
 var port = process.env.PORT || 8080;
 var ip = process.env.IP || '127.0.0.1';
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   console.log('===New header===')
   console.log(req.headers);
-  
+
   var out = {}
   out.ipaddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-  
+
   // get first language listed (before the comma)
-  var comma = req.headers['accept-language'].indexOf(',')
-  if (comma == -1){
-    out.language = req.headers['accept-language']
-  } else {
-    out.language = req.headers['accept-language'].substr(0,comma)
+  if (req.headers['accept-language']) {
+    var comma = req.headers['accept-language'].indexOf(',')
+    if (comma == -1) {
+      out.language = req.headers['accept-language']
+    }
+    else {
+      out.language = req.headers['accept-language'].substr(0, comma)
+    }
   }
-  
+  else {
+    out.language = 'unknown'
+  }
+
   // get OS from inside the first set of brackets in user-agent
-  var open = req.headers['user-agent'].indexOf('(')
-  var close = req.headers['user-agent'].indexOf(')')
-  out.software = req.headers['user-agent'].substring(open+1,close)
-  
+  if (req.headers['user-agent']) {
+    var open = req.headers['user-agent'].indexOf('(')
+    var close = req.headers['user-agent'].indexOf(')')
+    out.software = req.headers['user-agent'].substring(open + 1, close)
+  }
+  else {
+    out.software = 'unknown'
+  }
+
   console.log('--- Sent out ---')
   console.log(out)
   console.log('')
